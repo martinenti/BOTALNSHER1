d8ac73ca9d6fb31cfdd0b0b2c25e8f7ef1b5b3c4
martinenti <40662211+martinenti@users.noreply.github.com>
2018-07-17 17:34:43 +0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,61 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+
+╔╦╦╦═╦╗╔═╦═╦══╦═╗
+║║║║╩╣╚╣═╣║║║║║╩╣
+╚══╩═╩═╩═╩═╩╩╩╩═╝ 
+
+‏مطفر وما معك فلوس
+تريد دولارات عشان تشتري بيها العاب
+او اي شي 
+عندي لك الحل سيرفر مضمون جدا جدا جدا جدا معي تجربت ي الشخصيه والله مضمون 
+اقسم بالله لك مضمون ولو مو مضمون اخرج بس جرب
+هذا السيرفر اجنبي
+هذا رابط الدعوه 
+اريدك تدعيلي فقط
+:
+
+https://discord.gg/tKGfub3
+
+joined info-selfie
+! Selfie.                              
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+
+╔╦╦╦═╦╗╔═╦═╦══╦═╗
+║║║║╩╣╚╣═╣║║║║║╩╣
+╚══╩═╩═╩═╩═╩╩╩╩═╝ 
+
+‏مطفر وما معك فلوس
+تريد دولارات عشان تشتري بيها العاب
+او اي شي 
+عندي لك الحل سيرفر مضمون جدا جدا جدا جدا معي تجربتي الشخصيه والله مضمون 
+اقسم بالله لك مضمون ولو مو مضمون اخرج بس جرب
+هذا السيرفر اجنبي
+هذا رابط الدعوه 
+اريدك تدعيلي فقط
+:
+
+https://discord.gg/tKGfub3
+joined info-selfie
+! Selfie.                              
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+
+
+client.login(process.env.BOT_TOKEN);
