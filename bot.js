6d8d73d90d483f00b784f93e472f74294a7d3f3b
martinenti <40662211+martinenti@users.noreply.github.com>
2018-07-17 20:41:00 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,89 +11,50 @@ console.log("Welcome Again !");
 ║║║║╩╣╚╣═╣║║║║║╩╣
 ╚══╩═╩═╩═╩═╩╩╩╩═╝ 
 
-‏مطفر وما معك فلوس
-تريد دولارات عشان تشتري بيها العاب
-او اي شي 
-عندي لك الحل سيرفر مضمون جدا جدا جدا جدا معي تجربت ي الشخصيه والله مضمون 
-اقسم بالله لك مضمون ولو مو مضمون اخرج بس جرب
-هذا السيرفر اجنبي
-هذا رابط الدعوه 
-اريدك تدعيلي فقط
-:
-
-https://discord.gg/tKGfub3
-
-joined info-selfie
-! Selfie.                              
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
-**`)
+ تبي تشتري بوت عام مثل برو بوت
+بيه:
+اغاني
+العاب
+اوامر عامه
+اوامر اداريه
+شغال 24 ساعه 
+طول الحياه\
++ 
+هديه بوت نشر شغال 24 ساعه طول الحياه
+العسر 30 الف كريديت برو بوت فقط 
+لو تريد كلمني
 }).catch(console.error)
 
 })
  
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
-  return channel.send(` 
-
+  return channel.send(`
+		      
+		      
 ╔╦╦╦═╦╗╔═╦═╦══╦═╗
 ║║║║╩╣╚╣═╣║║║║║╩╣
 ╚══╩═╩═╩═╩═╩╩╩╩═╝ 
-
-‏مطفر وما معك فلوس
-تريد دولارات عشان تشتري بيها العاب
-او اي شي 
-عندي لك الحل سيرفر مضمون جدا جدا جدا جدا معي تجربتي الشخصيه والله مضمون 
-اقسم بالله لك مضمون ولو مو مضمون اخرج بس جرب
-هذا السيرفر اجنبي
-هذا رابط الدعوه 
-اريدك تدعيلي فقط
-:
-
-https://discord.gg/tKGfub3
-joined info-selfie
-! Selfie.                              
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
-**`) 
+تبي تشتري بوت عام مثل برو بوت
+بيه:
+اغاني
+العاب
+اوامر عامه
+اوامر اداريه
+شغال 24 ساعه 
+طول الحياه\
++ 
+هديه بوت نشر شغال 24 ساعه طول الحياه
+العسر 30 الف كريديت برو بوت فقط 
+لو تريد كلمني
 }).catch(console.error)
+
 })
+ 
 
 
 
 
-const botStats = {
-	totalGuildsID: '468779433692758016',
-	totalUsersID: '468779034302480384',
-	totalChannelsID: '468779680632274963',
-	
-	
-}
-	
-	client.on('guildCreate', guild => {
-		
-	client.channels.get(botStats.totalGuildsID).setname('TotalGuilds : ${client.guilds.size}');
-	client.channels.get(botStats.totalUsersID).setname('TotalUsers : ${client.guilds.reduce((a, g) => a + g.memberCount, 0)}');
-	client.channels.get(botStats.totalChannelsID).setname('TotalChannels : ${client.channels.size}');
-		
-		
-		
-});
-		
-		
-	client.on('guildDelete', guild => {
-	
-	
-	client.channels.get(botStats.totalGuildsID).setname('TotalGuilds : ${client.guilds.size}');
-	client.channels.get(botStats.totalUsersID).setname('TotalUsers : ${client.guilds.reduce((a, g) => a + g.memberCount, 0)}');
-	client.channels.get(botStats.totalChannelsID).setname('TotalChannels : ${client.channels.size}');
-		
-			
-		
-	
-	
-	
-	
-	
-	});
 
 
 client.login(process.env.BOT_TOKEN);
